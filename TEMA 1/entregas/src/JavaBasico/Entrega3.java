5d73ef5c4f9adb4160b005ecb7a2b98859b1a780
Diego Vazquez <diegovazquezok@gmail.com>
2023-04-17 17:10:21 -0300

    Entrega 3 Terminada

---
+++ TEMA 1/entregas/src/JavaBasico/Entrega3.java
@@ -0,0 +1,13 @@
+package JavaBasico;
+
+public class Entrega3 {
+
+    public static void main(String[] args) {
+        String[] nombres = {"Julio", "Rosa", "Maria", "Raul"};
+        String resultado = "";
+        for (int i = 0; i < nombres.length; i++) {
+            resultado += nombres[i] + " ";
+        }
+        System.out.println(resultado);
+    }
+}
