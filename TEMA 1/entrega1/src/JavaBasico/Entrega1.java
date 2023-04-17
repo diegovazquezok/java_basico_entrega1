7bc283655e77c51aa9abf8d7dfded4d1acf6aa94
Diego Vazquez <diegovazquezok@gmail.com>
2023-04-17 15:16:03 -0300

    Entrega 1 Terminada

---
+++ TEMA 1/entrega1/src/JavaBasico/Entrega1.java
@@ -0,0 +1,29 @@
+package JavaBasico;
+
+public class Entrega1 {
+
+    /*  Crea un proyecto de Java desde 0
+
+        Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una clase.
+
+        Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se han visto en las sesiones.
+
+        Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String. */
+
+    public static void main(String[] args) {
+
+        int variable1 = 15;
+        long variable2 = 150;
+        double variable3 = 10.6;
+        boolean variable4 = true;
+        String variable5 = "Hola Mundo";
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+
+
+    }
+}
